8033372eb13538540035d74b915423bac9df704b
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-09-27 09:12:36 -0700

    testing

--- hw4/part1.js
+++ hw4/part1.js
@@ -2,23 +2,37 @@ window.onload = DoThings;
 
 function DoThings() {
     let r_1 = Range(5, 25, 4);
-    let r_2 = Range(3, 18, 3);
     var el_sum1 = document.getElementById("sum_1");
+    el_sum1.innerHTML = Sum(r_1).toLocaleString("en-US");
     var el_prd1 = document.getElementById("prd_1");
+    el_prd1.innerHTML = Product(r_1).toLocaleString("en-US");
+
+    let r_2 = Range(3, 18, 3);
     var el_sum2 = document.getElementById("sum_2");
+    el_sum2.innerHTML = Sum(r_2).toLocaleString("en-US");
     var el_prd2 = document.getElementById("prd_2");
+    el_prd2.innerHTML - Product(r_2).toLocaleString("en-US")
 }
 
 function Range(lower, upper, delta) {
     let range = [];
     for (let i=lower; i<=upper; i+=delta) {
         range.push(i);
-        console.log(`adding ${i}`);
     }
+    let i = lower;
+    range = [i]
+    while(i<=upper) {
+        range.push(i+=delta);
+    }
+    range.forEach((el) => console.log(el))
     return range;
 }
 function Product(arr) {
-
+    let p = 0
+    return 123456789;
+}
+function Sum(arr) {
+    return 987654321;
 }
 
 function FunkyText() {
