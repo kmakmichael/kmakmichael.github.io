3edce9f9f1d71265e87559edd6e1f0366e1e2206
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-09-27 09:21:56 -0700

    h4p1: restructuring

--- hw4/part1.js
+++ hw4/part1.js
@@ -1,11 +1,13 @@
 window.onload = DoThings;
 
 function DoThings() {
-    let r_1 = Range(5, 25, 4);
+    let plus = (a, b) => a + b;
+    let mult = (a, b) => a * b;
+
     var el_sum1 = document.getElementById("sum_1");
-    el_sum1.innerHTML = Sum(r_1).toLocaleString("en-US");
+    el_sum1.innerHTML = FoldFor(5, 25, 4, plus).toLocaleString("en-US");
     var el_prd1 = document.getElementById("prd_1");
-    el_prd1.innerHTML = Product(r_1).toLocaleString("en-US");
+    el_prd1.innerHTML = FoldFor(5, 25, 4, mult).toLocaleString("en-US");
 
     let r_2 = Range(3, 18, 3);
     var el_sum2 = document.getElementById("sum_2");
@@ -13,46 +15,23 @@ function DoThings() {
     var el_prd2 = document.getElementById("prd_2");
     el_prd2.innerHTML - Product(r_2).toLocaleString("en-US")
 }
+function FoldFor(lower, upper, increment, operation) {
+    let result = 0;
+    for (let i=lower; i<=upper; i+= increment) {
+        i = operation(result, i);
+    }
+}
 
 function Range(lower, upper, delta) {
     let range = [];
     for (let i=lower; i<=upper; i+=delta) {
         range.push(i);
     }
-    let i = lower;
-    range = [i]
-    while(i<=upper) {
-        range.push(i+=delta);
-    }
-    range.forEach((el) => console.log(el))
     return range;
 }
 function Product(arr) {
-    let p = 0
     return 123456789;
 }
 function Sum(arr) {
     return 987654321;
-}
-
-function FunkyText() {
-    var elmt = document.getElementById("fancyText");
-
-    var pair1 = document.createElement("span");
-    pair1.innerHTML = "xy";
-    pair1.style = "color:red;font-weight:strong;font-family:\"Times New Roman\";";
-    
-    var pair2 = document.createElement("span");
-    pair2.innerHTML = "12";
-    pair2.style = "color:blue;font-family:Arial;";
-
-    var pair3 = document.createElement("span");
-    pair3.innerHTML = "89";
-    pair3.style = "color:green;font-style:italic;font-family:Impact;";
-    
-    elmt.appendChild(pair1);
-    elmt.innerHTML += "&nbsp;";
-    elmt.appendChild(pair2);
-    elmt.innerHTML += "&nbsp;";
-    elmt.appendChild(pair3);
 }
