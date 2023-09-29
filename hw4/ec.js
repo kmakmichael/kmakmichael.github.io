c1ec0cb249f5d9bdc2f93a97e2084fddeaeaf975
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-09-29 09:14:55 -0700

    h4ec: adjust breaking

--- hw4/ec.js
+++ hw4/ec.js
@@ -15,11 +15,11 @@ function ButtonClick() {
     let sz = parseFloat(document.forms["cube_form"].elements["cube_size"].value);
     let cube_p = document.getElementById("cube");
     if (Number.isNaN(sz)) {
-        cube_p.innerHTML = "Invalid input (must be a number)" + "<br />".repeat(9);
+        cube_p.innerHTML = "Invalid input (must be a number)" + "<br />".repeat(10);
         return;
     }
     if (sz < 2 || sz > 10) {
-        cube_p.innerHTML = "Number out of range (must be between 2 and 10 inclusive)" + "<br />".repeat(9);
+        cube_p.innerHTML = "Number out of range (must be between 2 and 10 inclusive)" + "<br />".repeat(10);
         return;
     }
     DrawCube(sz);
