44b7d1c0dcf23ca1a0998670ee5d448619953d23
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-09-29 08:58:08 -0700

    h4ec: tweaks

--- hw4/ec.js
+++ hw4/ec.js
@@ -1,19 +1,5 @@
-function ButtonClick() {
-    let sz = parseFloat(document.forms["cube_form"].elements["cube_size"].value);
-    let cube_p = document.getElementById("cube");
-    if (Number.isNaN(sz)) {
-        cube_p.innerHTML = "Invalid input (must be a number)";
-        return;
-    }
-    if (sz < 2 || sz > 10) {
-        cube_p.innerHTML = "Number out of range (must be between 2 and 10 inclusive)";
-        return;
-    }
-    DrawCube(sz);
-}
-
 function DrawCube(sz) {
-    let ws = "&ensp;".repeat(sz) + "&nbsp;";
+    let ws = "&ensp;".repeat(sz-2) + "&nbsp;";
     let edge = "* ".repeat(sz-1) + "*<br />"
     let mid = "*" + ws + "*" + "<br />";
 
@@ -21,4 +7,19 @@ function DrawCube(sz) {
     cube_p.innerHTML += edge;
     cube_p.innerHTML += mid.repeat(sz - 2);
     cube_p.innerHTML += edge;
+    cube_p.innerHTML += "<br />".repeat(10 - sz);
+}
+
+function ButtonClick() {
+    let sz = parseFloat(document.forms["cube_form"].elements["cube_size"].value);
+    let cube_p = document.getElementById("cube");
+    if (Number.isNaN(sz)) {
+        cube_p.innerHTML = "Invalid input (must be a number)" + "<br />".repeat(9);
+        return;
+    }
+    if (sz < 2 || sz > 10) {
+        cube_p.innerHTML = "Number out of range (must be between 2 and 10 inclusive)" + "<br />".repeat(9);
+        return;
+    }
+    DrawCube(sz);
 }
