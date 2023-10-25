959f53e25157cd275271131285754ea3bc87bd49
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-10-25 09:53:55 -0700

    h5p1: hopefully jquery

--- hw5/part1.js
+++ hw5/part1.js
@@ -1,5 +1,6 @@
-DoThings();
-
-function DoThings() {
-    console.log("part 1");
-}
+$("form").on("submit", function() {
+    event.preventDefault();
+    let clean = true;
+    clean = $(this).find('[name=full_name]').val() != "";
+    console.log(clean);
+});
