96bcf9864cff10ac6d699730fc26f8bcdd54329c
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-10-25 10:18:45 -0700

    h5p1: is this how it works

--- hw5/part1.js
+++ hw5/part1.js
@@ -1,6 +1,7 @@
-$("form").on("submit", function() {
+$("form").on("submit", function(event) {
     event.preventDefault();
     let clean = true;
     clean = $(this).find('[name=full_name]').val() != "";
+    console.log($(this).find('[name=browsers]'))
     console.log(clean);
 });
