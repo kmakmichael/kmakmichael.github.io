5d47569d235013c8a1373dfd0cc85c29018ed1cc
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-10-25 10:46:15 -0700

    h5p1: testing :checked

--- hw5/part1.js
+++ hw5/part1.js
@@ -3,6 +3,6 @@ $("form").on("submit", function(event) {
     let clean = true;
     clean = $(this).find('[name=full_name]').val() != "";
     console.log()
-    console.log($(this).find('[name=browsers]').val());
+    console.log($(this).find('input[name=browsers]:checked').val());
     console.log(clean);
 });
