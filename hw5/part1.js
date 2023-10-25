b09c669afad4d2e60e17b827a3f49b1a9e411cac
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-10-25 10:26:12 -0700

    h5p1: or this maybe

--- hw5/part1.js
+++ hw5/part1.js
@@ -2,6 +2,7 @@ $("form").on("submit", function(event) {
     event.preventDefault();
     let clean = true;
     clean = $(this).find('[name=full_name]').val() != "";
-    console.log($(this).find('[name=browsers]'))
+    console.log()
+    console.log($(this).find('[name=browsers]').val());
     console.log(clean);
 });
