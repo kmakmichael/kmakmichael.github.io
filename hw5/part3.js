5cda27f792cecd85bf9f450611c29107815b78ff
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-11-03 08:51:56 -0700

    h5p3: adjust names

--- hw5/part3.js
+++ hw5/part3.js
@@ -1,4 +1,4 @@
-document.forms.state_form.submit.addEventListener("submit", FormSubmit);
+document.forms.p3form.submit.addEventListener("submit", FormSubmit);
 
 function FormSubmit() {
     console.log("state submitted");
