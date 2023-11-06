34c42f197371b5ee56b672af12634f8d7a5c1b26
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-11-06 09:35:45 -0800

    h5p2: open new page

--- hw5/part2.js
+++ hw5/part2.js
@@ -1,4 +1,10 @@
 
-function goToNewPage(frm) {
-    console.log(frm);
+function goToNewPage(myForm) {
+    var mydest = myForm.destList.options[myForm.destList.selectedIndex].value;
+    console.log(mydest);
+    if (mydest == ""){
+        console.log("skipping")
+    } else {
+        window.open(mydest, '_blank');
+    }
 }
