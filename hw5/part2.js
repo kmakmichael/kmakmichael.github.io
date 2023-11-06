c97a67b04f749b3ebaf6b95b7dc7bec7165c3944
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-11-06 09:46:25 -0800

    h5p2: blank option, remove prints

--- hw5/part2.js
+++ hw5/part2.js
@@ -1,10 +1,7 @@
 
 function goToNewPage(myForm) {
     var mydest = myForm.destList.options[myForm.destList.selectedIndex].value;
-    console.log(mydest);
-    if (mydest == ""){
-        console.log("skipping")
-    } else {
+    if (mydest != "") {
         window.open(mydest, '_blank');
     }
 }
