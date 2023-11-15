85366b53e344b842756c70213c8103ddf5fee5c8
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-11-15 08:55:46 -0800

    h6p1: submit&reset buttons

--- hw6/part1.js
+++ hw6/part1.js
@@ -1,28 +1,4 @@
-$("form").on("submit", function(event) {
+document.forms[0].onsubmit = (event) => {
+    console.log("submission");
     event.preventDefault();
-    let msg = ""
-    if ($(this).find("input[name=full_name]").val() == "") {
-        msg += "Please enter a name<br>";
-    }
-    if ($(this).find("input[name=age_group]:checked").length <= 0) {
-        msg += "Please select an age<br />";
-    }
-    if ($(this).find("input[name=browsers]:checked").length <= 0) {
-        msg += "Please select at least one browser<br />";
-    }
-    if ($(this).find("select[name=movie]").val() == "") {
-        msg += "Please select a movie genre<br />";
-    }
-    if (msg == "") {
-        $("p#form_output").html("<b>Thanks, your data was submitted.</b>");
-    } else {
-        $("p#form_output").html("<b>Form data not submitted. Errors:</b><br />" + msg);
-    }
-});
-
-function ClearForm() {
-    $("form").find("input[name=full_name]").val("");
-    $("form").find("input:checked").prop("checked", false);
-    $("form").find("select[name=movie]").val("");
-    $("p#form_output").html("");
 }
