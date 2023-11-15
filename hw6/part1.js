c7e83188f7122acfa84c83cadeb9a1a7480cd474
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-11-15 09:47:05 -0800

    h6p1: spacing and rounding

--- hw6/part1.js
+++ hw6/part1.js
@@ -1,16 +1,14 @@
 var f = document.forms[0];
 
 f.onsubmit = (event) => {
-    console.log("submission");
     event.preventDefault();
     let val = f.elements["num_in"].value;
     let num = parseFloat(val);
     msg = `You typed ${val}`
-    msg += `Rounded to the nearest integer: ${num.toFixed()}`;
-    msg += `Square root rounded to integer: ${Math.sqrt(num).toFixed()}`;
-    msg += `Rounded to the nearest 10th: ${num.toFixed(1)}`;
-    msg += `Rounded to the nearest 100th: ${num.toFixed(2)}`;
-    msg += `Rounded to the nearest 1000th: ${num.toFixed(3)}`;
-
+    msg += `\nRounded to the nearest integer: ${num.toFixed()}`;
+    msg += `\nSquare root rounded to integer: ${Math.sqrt(num).toFixed()}`;
+    msg += `\nRounded to the nearest 10th: ${(Math.round(num * 10.0) / 10.0).toFixed(1)}`;
+    msg += `\nRounded to the nearest 100th: ${(Math.round(num * 100.0) / 100.0).toFixed(2)}`;
+    msg += `\nRounded to the nearest 1000th: ${(Math.round(num * 1000.0) / 1000.0).toFixed(3)}`;
     f.elements["txt_out"].value = msg;
 }
