fdba2bb5036cef09e25248693f1f2fe6abd6b963
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-11-15 09:16:12 -0800

    h6p1: js

--- hw6/part1.js
+++ hw6/part1.js
@@ -1,4 +1,14 @@
-document.forms[0].onsubmit = (event) => {
+var f = document.forms[0];
+
+f.onsubmit = (event) => {
     console.log("submission");
     event.preventDefault();
+    let val = f.elements["num_in"].value;
+    let num = parseFloat(val);
+    msg = `You typed ${val}`
+    msg += `Rounded to the nearest integer: ${num.toFixed()}`;
+    msg += `Square root rounded to integer: ${Math.sqrt(num).toFixed()}`;
+    msg += `Rounded to the nearest 10th: ${num.toFixed(1)}`;
+    msg += `Rounded to the nearest 100th: ${num.toFixed(2)}`;
+    msg += `Rounded to the nearest 1000th: ${num.toFixed(3)}`;
 }
