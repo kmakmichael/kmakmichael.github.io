0d150ecd583c2c2c7360ac3502101dcd2c805258
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-04 10:48:18 -0800

    h7p1: change event test

--- hw7/part1.js
+++ hw7/part1.js
@@ -1,1 +1,10 @@
+var f = document.forms[0];
 
+f.elements["bg"].addEventListener("change", (event) => {
+    console.log("changing background");
+});
+
+
+f.st_b.addEventListener("change", (event) => {
+    console.log("bolding");
+});
