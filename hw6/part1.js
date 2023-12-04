a4bac25503a0abf7b38675c0573ff88872d1bc04
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-04 07:51:59 -0800

    h6p1: possible fix

--- hw6/part1.js
+++ hw6/part1.js
@@ -1,15 +1,19 @@
 var f = document.forms[0];
-f.elements["num_in"].setCustomValidity("Please enter a number with 4 decimal places");
 
 f.onsubmit = (event) => {
     event.preventDefault();
     let val = f.elements["num_in"].value;
-    let num = parseFloat(val);
-    msg = `You typed ${val}`
-    msg += `\nRounded to the nearest integer: ${num.toFixed()}`;
-    msg += `\nSquare root rounded to integer: ${Math.sqrt(num).toFixed()}`;
-    msg += `\nRounded to the nearest 10th: ${(Math.round(num * 10.0) / 10.0).toFixed(1)}`;
-    msg += `\nRounded to the nearest 100th: ${(Math.round(num * 100.0) / 100.0).toFixed(2)}`;
-    msg += `\nRounded to the nearest 1000th: ${(Math.round(num * 1000.0) / 1000.0).toFixed(3)}`;
-    f.elements["txt_out"].value = msg;
+    if (val.search("\d*\.\d\d\d\d+") != -1) {
+        let num = parseFloat(val);
+        msg = `You typed ${val}`
+        msg += `\nRounded to the nearest integer: ${num.toFixed()}`;
+        msg += `\nSquare root rounded to integer: ${Math.sqrt(num).toFixed()}`;
+        msg += `\nRounded to the nearest 10th: ${(Math.round(num * 10.0) / 10.0).toFixed(1)}`;
+        msg += `\nRounded to the nearest 100th: ${(Math.round(num * 100.0) / 100.0).toFixed(2)}`;
+        msg += `\nRounded to the nearest 1000th: ${(Math.round(num * 1000.0) / 1000.0).toFixed(3)}`;
+        f.elements["txt_out"].value = msg;
+    } else {
+        console.log("invalid");
+        f.elements["txt_out"].value = "Please enter a number with 4 decimal places";
+    }
 }
