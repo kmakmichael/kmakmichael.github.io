0cd1fc1ba01635181ec60f919523b764790a6c6f
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-04 09:44:49 -0800

    h6p1: adjust regex

--- hw6/part1.js
+++ hw6/part1.js
@@ -4,7 +4,7 @@ f.onsubmit = (event) => {
     event.preventDefault();
     let val = f.elements["num_in"].value;
     console.log(val)
-    if (val.trim().search("^\d*\.\d\d\d\d+$") != -1) {
+    if (val.trim().search("^\d+\.\d\d\d\d+$") != -1) {
         let num = parseFloat(val);
         msg = `You typed ${val}`
         msg += `\nRounded to the nearest integer: ${num.toFixed()}`;
