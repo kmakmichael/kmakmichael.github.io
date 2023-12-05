7acbf8b01428c3365f6f22393dfa11b3d97447b4
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-05 11:10:11 -0800

    h6p2: printout

--- hw6/part2.js
+++ hw6/part2.js
@@ -3,17 +3,20 @@ $("form").on("submit", () => {
     let needle = $("input#needle").val();
     let haystack = $("textarea#haystack").val();
     let m = Find(needle, haystack);
-    console.log(`${m}`);
+    if (m > 0) {
+        $("p#printout").text(`Found ${needle} ${m} times in text.`);
+    } else {
+        $("p#printout").text("");
+    }
 });
 
 function Find(n, h) {
     let m = -1;
-    let idx = 0;
+    let idx = -1;
     let s = h;
     do {
         idx = h.indexOf(n, idx+1);
         ++m;
-        console.log(`found ${n} at ${idx} of ${h}`);
     } while (idx > -1)
     return m;
 }
