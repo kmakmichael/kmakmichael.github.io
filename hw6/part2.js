e48d544aff74451acedbf2739e10e05244f79a75
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-05 10:46:34 -0800

    h6p2: re-order

--- hw6/part2.js
+++ hw6/part2.js
@@ -3,17 +3,17 @@ $("form").on("submit", () => {
     let needle = $("input#needle").val();
     let haystack = $("textarea#haystack").val();
     let m = Find(needle, haystack);
-    console.log(`${matches}`);
+    console.log(`${m}`);
 });
 
 function Find(n, h) {
     let m = -1;
-    let idx = -1;
+    let idx = 0;
     do {
-        console.log(`finding ${n} in ${h}`);
-        idx = h.indexOf(m);
-        ++m;
         h = h.substring(idx);
+        console.log(`finding ${n} in ${h}`);
+        idx = h.indexOf(n);
+        ++m;
     } while (idx > -1)
     return m;
 }
