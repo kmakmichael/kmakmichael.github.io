b7a186fde64ea3ed613c0b0ed324516b55e9076d
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-05 09:45:34 -0800

    h7p2:????

--- hw7/part2.js
+++ hw7/part2.js
@@ -1,9 +1,18 @@
 var cookies = ReadCookies();
 
-if ("name" in cookies && "username" in cookies) {
-    let cb = document.querySelector("#cookiebox");
-    cb.innerHTML = `<p>Welcome back, ${cookies.name}</p>`;
-}
+$(document).ready(() => {
+    if ("name" in cookies && "username" in cookies) {
+        let cb = document.querySelector("#cookiebox");
+        cb.innerHTML = `<p>Welcome back, ${cookies.name}</p>`;
+    }
+    
+    var f = document.forms[0]
+    f.onsubmit = (event) => {
+        CreateCookie("name", f.ck_name.value);
+        CreateCookie("username", f.ck_user.value);
+    }
+});
+
 
 function CreateCookie(name, val) {
     document.cookie = encodeURIComponent(`${name}=${val};`);
@@ -21,11 +30,3 @@ function ReadCookies() {
     }
     return c;
 }
-
-$(document).ready(() => {
-    var f = document.forms[0]
-    f.onsubmit = (event) => {
-        CreateCookie("name", f.ck_name.value);
-        CreateCookie("username", f.ck_user.value);
-    }
-});
