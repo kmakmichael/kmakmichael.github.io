e85746d6dce137b0f1e614ee393e022b35998ebc
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-05 09:49:24 -0800

    h7p2: actually use jquery

--- hw7/part2.js
+++ hw7/part2.js
@@ -2,9 +2,8 @@ var cookies = ReadCookies();
 
 $(document).ready(() => {
     if ("name" in cookies && "username" in cookies) {
-        let cb = document.querySelector("#cookiebox");
-        cb.innerHTML = `<p>Welcome back, ${cookies.name}</p>`;
-    }
+        $("#cookiebox").innerHTML = `<p>Welcome back, ${cookies.name}</p>`;
+    } else { }
     
     var f = document.forms[0]
     f.onsubmit = (event) => {
