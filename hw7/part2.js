6fc025df6a5de81439cadd98d9d343eef44895e2
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-05 09:44:35 -0800

    h7p2: fix onsubmit

--- hw7/part2.js
+++ hw7/part2.js
@@ -22,8 +22,10 @@ function ReadCookies() {
     return c;
 }
 
-var f = document.forms[0]
-f.onsubmit = (event) => {
-    CreateCookie("name", f.ck_name.value);
-    CreateCookie("username", f.ck_user.value);
-}
+$(document).ready(() => {
+    var f = document.forms[0]
+    f.onsubmit = (event) => {
+        CreateCookie("name", f.ck_name.value);
+        CreateCookie("username", f.ck_user.value);
+    }
+});
