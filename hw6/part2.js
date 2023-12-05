04010031faf97f6cb0558004f4ae47e75931b36f
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-05 10:43:43 -0800

    h6p2: try it

--- hw6/part2.js
+++ hw6/part2.js
@@ -2,6 +2,18 @@ $("form").on("submit", () => {
     event.preventDefault();
     let needle = $("input#needle").val();
     let haystack = $("textarea#haystack").val();
-    let matches = haystack.matchAll(`/${needle}/g`);
+    let m = Find(needle, haystack);
     console.log(`${matches}`);
 });
+
+function Find(n, h) {
+    let m = -1;
+    let idx = -1;
+    do {
+        console.log(`finding ${n} in ${h}`);
+        idx = h.indexOf(m);
+        ++m;
+        h = h.substring(idx);
+    } while (idx > -1)
+    return m;
+}
