fd5f54af457af8a3c44081c13908405fb1ad5fe5
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-06 07:54:12 -0800

    h6p3: revert that

--- hw6/part3.js
+++ hw6/part3.js
@@ -1,6 +1,3 @@
 $("document").ready(() => {
-    const input = document.querySelector("#pnum");
-    window.intlTelInput(input, {
-      utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
-    });
+    
 });
