89b8c8071b6783804bc0d1b4fa3dbbf3ac118ee6
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-06 08:56:40 -0800

    h6p3: re-order?

--- hw6/part3.js
+++ hw6/part3.js
@@ -3,10 +3,10 @@ $("document").ready(() => {
 });
 
 $("form").on("submit", () => {
+    event.preventDefault();
     console.log($("input#pnum").val().trim());
     let match = $("input#pnum").val().trim().match(/\(([0-9]{3})\) ([0-9]{3})-([0-9]{4})/);
     $("input#area").val(match[1]);
     $("input#area").val(match[2]);
     $("input#area").val(match[3]);
-    event.preventDefault();
 })
