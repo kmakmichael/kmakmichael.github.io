8dd83defe9d6d20a38988839ea636384255afb3a
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-06 08:54:05 -0800

    h6p3: match

--- hw6/part3.js
+++ hw6/part3.js
@@ -3,6 +3,10 @@ $("document").ready(() => {
 });
 
 $("form").on("submit", () => {
-    console.log($("input#pnum").val());
+    console.log($("input#pnum").val().trim());
+    let match = $("input#pnum").val().trim().match(/\(([0-9]{3})\) ([0-9]{3})-([0-9]{4})/);
+    $("input#area").val(match[1]);
+    $("input#area").val(match[2]);
+    $("input#area").val(match[3]);
     event.preventDefault();
 })
