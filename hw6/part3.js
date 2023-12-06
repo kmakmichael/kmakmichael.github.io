ffb74f5a7e1e5d36471564208b6d4bcbb9663cc3
kmakmichael <70419730+kmakmichael@users.noreply.github.com>
2023-12-06 09:01:58 -0800

    h6p3: proper queries

--- hw6/part3.js
+++ hw6/part3.js
@@ -1,13 +1,13 @@
 $("document").ready(() => {
     $("input#pnum").mask("(000) 000-0000");
-    
+
     $("form").on("submit", (event) => {
         event.preventDefault();
         console.log($("input#pnum").val().trim());
         let match = $("input#pnum").val().trim().match(/\(([0-9]{3})\) ([0-9]{3})-([0-9]{4})/);
         console.log(match);
         $("input#area").val(match[1]);
-        $("input#area").val(match[2]);
-        $("input#area").val(match[3]);
+        $("input#prefix").val(match[2]);
+        $("input#line").val(match[3]);
     })
 });
